d7e0b20049b678f994ca6b74d5cc3a81af0491c6
Federico <brunetti.federico.13@gmail.com>
2025-03-31 17:19:41 +0200

    aggiunto qstNumber per calcolare numero domanda

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,29 +1,30 @@
 window.onload = function () {
   console.log("caricata!");
   const h2 = document.querySelector("h2");
-
+  const h6 = document.querySelector("h6");
+  console.dir(h6);
   const div = document.getElementById("btnArea");
-  console.log(div);
-  console.dir(div);
-
   const btn1 = document.getElementById("button1");
   const btn2 = document.getElementById("button2");
   const btn3 = document.getElementById("button3");
   const btn4 = document.getElementById("button4");
-  console.dir(btn1);
+
+  let qstNumber = 0;
 
   fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
     .then((response) => response.json())
     .then((data) => {
-      // for (let i = 0; i<= data.response.length; i++){}
-      const question = data.results[0].question;
+      /* for (let i = 0; i<= data.response.length; i++){
+      qstNumber =i;
+      } */
+
+      const question = data.results[qstNumber].question;
       h2.innerText = question;
-      console.log(question);
-      console.log(data.results[0].incorrect_answers[0]);
-      btn1.innerText = data.results[0].incorrect_answers[0];
-      btn2.innerText = data.results[0].incorrect_answers[1];
-      btn4.innerText = data.results[0].correct_answer;
-      btn3.innerText = data.results[0].incorrect_answers[2];
+      btn1.innerText = data.results[qstNumber].incorrect_answers[0];
+      btn2.innerText = data.results[qstNumber].incorrect_answers[1];
+      btn3.innerText = data.results[qstNumber].incorrect_answers[2];
+      btn4.innerText = data.results[qstNumber].correct_answer;
     })
     .catch((error) => console.error("Errore nel caricamento:", error));
+  h6.innerText = `QUESTION ${qstNumber + 1}`;
 };
