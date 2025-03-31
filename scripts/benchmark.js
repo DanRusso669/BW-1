44fa293a7de8e16b5737aebf61f1b1eeedd5b930
Federico <brunetti.federico.13@gmail.com>
2025-03-31 17:03:23 +0200

    fetch completato

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,13 +1,29 @@
-console.log("caricata!");
-const h2 = document.querySelector("h2");
+window.onload = function () {
+  console.log("caricata!");
+  const h2 = document.querySelector("h2");
 
-fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
-  .then((response) => response.json())
-  .then((data) => {
-    // for (let i = 0; i<= data.response.lenght; i++){
+  const div = document.getElementById("btnArea");
+  console.log(div);
+  console.dir(div);
 
-    // }
-    const question = data.results[0].question;
-    h2.innerText = question;
-  })
-  .catch((error) => console.error("Errore nel caricamento:", error));
+  const btn1 = document.getElementById("button1");
+  const btn2 = document.getElementById("button2");
+  const btn3 = document.getElementById("button3");
+  const btn4 = document.getElementById("button4");
+  console.dir(btn1);
+
+  fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
+    .then((response) => response.json())
+    .then((data) => {
+      // for (let i = 0; i<= data.response.length; i++){}
+      const question = data.results[0].question;
+      h2.innerText = question;
+      console.log(question);
+      console.log(data.results[0].incorrect_answers[0]);
+      btn1.innerText = data.results[0].incorrect_answers[0];
+      btn2.innerText = data.results[0].incorrect_answers[1];
+      btn4.innerText = data.results[0].correct_answer;
+      btn3.innerText = data.results[0].incorrect_answers[2];
+    })
+    .catch((error) => console.error("Errore nel caricamento:", error));
+};
