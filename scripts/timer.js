7ebd857e3d54b7d6fcc78a6240fdf306dbde6ab9
Dan Russo <rdaniele669@gmail.com>
2025-04-02 13:02:42 +0200

    adds timer's js and html
    Ho aggiunto il timer all'interno della benchmark page. Manca il css

---
+++ scripts/timer.js
@@ -0,0 +1,22 @@
+const sec = 11;
+
+const setTime = sec * 1000; // trasforma i secondi in millisec
+const startTime = Date.now(); // prende i millesec dal 01/01/70
+const futureTime = startTime + setTime; //somma i due valori sopra
+
+const countDownTimer = function () {
+  const currentTime = Date.now(); // riprende i millisec dal 01/01/70, ma essendo dentro la funzione che si ripete a loop, cambiano col passare dei secondi reali
+  const remainingTime = futureTime - currentTime; // calcola la differenza col tempo che passa
+  const timerText = document.getElementById("middleText"); // selezioniamo la variabile del testo
+
+  if (remainingTime <= 0) {
+    // se i tempo rimanente è uguale a 0
+    clearInterval(timerLoop); // Ferma il timer
+    timerText.innerText = "00"; // Timer scaduto quindi rimettiamo 00 sul testo
+  } else {
+    const secs = Math.floor((remainingTime / 1000) % 60).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
+    timerText.innerText = secs; // Mostra il tempo rimanente
+  }
+};
+
+const timerLoop = setInterval(countDownTimer);
