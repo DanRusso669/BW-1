8442318d74107eb3ac71f899cccef251ed6ef988
Alessio Panconi <apanconi94@gmail.com>
2025-04-03 14:33:58 +0200

    Fixes to feedback and welcome page
    Modifica pagina finale feedback con aggiunta bottoni + allineamento in welcome page della checkbox al label

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -60,13 +60,40 @@ function submitFeedBack() {
   let newP = document.createElement("p");
 
   if (nStelle < 6) {
-    newP.innerText = "Thanks for your feedback, you left a negative review 😢.";
+    newP.innerText = "Thanks for your feedback, you left a negative review, we will try to improve😢.";
   } else if (nStelle < 9) {
     newP.innerText = "Thanks for your feedback, you left a positive review 😊.";
   } else {
     newP.innerText = "Thanks for your feedback, you left a very positive review 🤩.";
   }
 
+  let newP2 = document.createElement("p");
+  newP2.innerText = "What do you want to do now?";
+
+  let divButtons = document.createElement("div");
+  divButtons.id = "divButtons";
+
+  let button1 = document.createElement("button");
+  button1.classList.add("button1");
+  button1.innerText = "Try the exam again";
+
+  button1.addEventListener("click", function () {
+    window.location.href = "./index.html";
+  });
+
+  let button2 = document.createElement("button");
+  button2.classList.add("button2");
+  button2.innerText = "Epicode Courses";
+
+  button2.addEventListener("click", function () {
+    window.open("https://epicode.com/it/corsi/", "_blank");
+  });
+
   newDiv.appendChild(newP);
   document.body.appendChild(newDiv);
+
+  divButtons.appendChild(newP2);
+  divButtons.appendChild(button1);
+  divButtons.appendChild(button2);
+  document.body.appendChild(divButtons);
 }
