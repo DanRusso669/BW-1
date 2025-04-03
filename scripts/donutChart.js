92c2ff811264e259a31cc2aba78dd7e45a69d34d
Dan Russo <rdaniele669@gmail.com>
2025-04-03 10:56:49 +0200

    adds opacity to stroke

--- scripts/donutChart.js
+++ scripts/donutChart.js
@@ -1,14 +1,14 @@
 // Donut chart results page
 
-const rightAnswers = 5; // risposte giuste
-const worngAnswers = 5; // risposte sbagliate
+const rightAnswers = 9; // risposte giuste
+const worngAnswers = 1; // risposte sbagliate
 const numberOfQuestions = 10; // numero domande
 
 const rightAnswersSlice = Math.floor((rightAnswers / numberOfQuestions) * 100); // percentuale delle risposte giuste senza simbolo %
 const wrongAnswersSlice = Math.floor((worngAnswers / numberOfQuestions) * 100); // percentuale delle risposte sbagliate senza simbolo %
 
-// console.log(wrongAnswersSlice);
-// console.log(rightAnswersSlice);
+console.log(wrongAnswersSlice);
+console.log(rightAnswersSlice);
 
 const percentageRightAnswers = (rightAnswers / numberOfQuestions).toLocaleString("en", {
   // percentuale delle risposte giuste con simbolo %
@@ -19,8 +19,8 @@ const percentageWrongAnswers = (worngAnswers / numberOfQuestions).toLocaleString
   style: "percent",
 });
 
-// console.log(percentageWrongAnswers);
-// console.log(percentageRightAnswers);
+console.log(percentageWrongAnswers);
+console.log(percentageRightAnswers);
 
 const circle = document.getElementById("donut-segment");
 circle.style.strokeDasharray = `${wrongAnswersSlice} ${rightAnswersSlice}`; // cambio delle porzioni del donut chart in base alla percentuale
