49113c311d574f2f98c720e8d0c45e7495a62b47
Dan Russo <rdaniele669@gmail.com>
2025-04-03 17:14:33 +0200

    adds name fixes

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -4,102 +4,103 @@ window.onload = function () {
   const h1 = document.querySelector("h1");
   const h4 = document.querySelector("h4");
   const btnArea = document.getElementById("btnArea");
-  
 
   let qstNumber = 0; //mi serve per avanzare correttamente nell'array
   let allQuestions = []; //array di oggetti in cui metterò le domande
   let currentQuestion; // prende la domanda corrente
-  let answers= []; //prende le risposte così come sono fornite dall'api
+  let answers = []; //prende le risposte così come sono fornite dall'api
   let mixedAnswer = []; //risposte con posizione randomizzata
-  let correctAnswers =0; //contatore di risposte corrette
+  let correctAnswers = 0; //contatore di risposte corrette
 
   fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy") //mi collego alle domande genrate dal server tramite API
-    .then((response) => response.json()) //converto le info json trovate nel link in formato js
-    .then((data) => {
+    .then(response => response.json()) //converto le info json trovate nel link in formato js
+    .then(data => {
       allQuestions = data.results; //metto tutte le domande del file json, con tutte le info, nell'array di oggetti creato prima
 
       console.log("domande raccolte: ", allQuestions); //conferma
-      loadQuestion(); //carico la funzione primaria di gererazione domande 
+      loadQuestion(); //carico la funzione primaria di gererazione domande
       consoleQuestion(); //carica la funzione del console log, utile per verificare quali domande compariranno.
     })
-    .catch((error) => console.error("Errore nel caricamento del file:", error)); //cattura l'errore in caso di fallimento con il collegamento al server
-  
-    
-    function consoleQuestion() {  //ci fa capire quali domande usciranno
+    .catch(error => console.error("Errore nel caricamento del file:", error)); //cattura l'errore in caso di fallimento con il collegamento al server
+
+  function consoleQuestion() {
+    //ci fa capire quali domande usciranno
     for (let i = 0; i < allQuestions.length; i++) {
       console.log(`tipo: ${allQuestions[i].type},  corretta: ${allQuestions[i].correct_answer} \n ${allQuestions[i].question}`);
     }
   }
 
-  function loadQuestion() { //funzione principale,che si avvia quando si fetcha e poi richiamata ogni volta che si preme il bottone 
-    
-    currentQuestion = allQuestions[qstNumber]; //prendo la domanda corrente
-    h1.innerText = currentQuestion.question;
+  function loadQuestion() {
+    //funzione principale,che si avvia quando si fetcha e poi richiamata ogni volta che si preme il bottone
 
+    currentQuestion = allQuestions[qstNumber]; //prendo la domanda corrente
+    const question = document.getElementById("question");
+    question.innerText = currentQuestion.question;
+    const elenco = document.getElementById("numberOfQuestion");
 
     answers = [currentQuestion.correct_answer, ...currentQuestion.incorrect_answers]; //prendo tutte le risposte
-    if (currentQuestion.type !=="boolean"){ 
-     mixedAnswer = shuffleArray(answers);
-    }else if (currentQuestion.correct_answer=== "False"){ //se la risposta "false" è quella vera, in automatico la metterebbe per primoe e in questo modo glielo impediamo spostandola come seconda posizione
-        mixedAnswer[0] =answers[1];
-        mixedAnswer[1] =answers[0];
-
-      
-    }else {//se è boolean e la risposta giusta è "true"; allora non fare alcun cambiamento sulla posizione
-      mixedAnswer= answers; 
+    if (currentQuestion.type !== "boolean") {
+      mixedAnswer = shuffleArray(answers);
+    } else if (currentQuestion.correct_answer === "False") {
+      //se la risposta "false" è quella vera, in automatico la metterebbe per primoe e in questo modo glielo impediamo spostandola come seconda posizione
+      mixedAnswer[0] = answers[1];
+      mixedAnswer[1] = answers[0];
+    } else {
+      //se è boolean e la risposta giusta è "true"; allora non fare alcun cambiamento sulla posizione
+      mixedAnswer = answers;
     }
-  
-    h4.innerText = `QUESTION ${qstNumber + 1} /10`;
+
+    elenco.innerText = `QUESTION ${qstNumber + 1} /10`;
     btnArea.innerHTML = "";
 
-    for (let i = 0; i < mixedAnswer.length; i++) {// i bottoni vengono creati sulla base delle risposte mischiate
-      
+    for (let i = 0; i < mixedAnswer.length; i++) {
+      // i bottoni vengono creati sulla base delle risposte mischiate
+
       let button = document.createElement("button");
       button.innerText = mixedAnswer[i];
-     
-    
 
-    
-      button.addEventListener("click", function () { 
+      button.addEventListener("click", function () {
         let userResponse = button.innerText;
-        console.log (`Risposta scelta: ${userResponse}, risposta corretta: ${currentQuestion.correct_answer}`);
+        console.log(`Risposta scelta: ${userResponse}, risposta corretta: ${currentQuestion.correct_answer}`);
         answerVerify(userResponse, currentQuestion.correct_answer);
-        console.log (correctAnswers);
+        console.log(correctAnswers);
         qstNumber++;
         if (qstNumber < allQuestions.length) {
           loadQuestion();
         } else {
           theEnd();
         }
-       }); 
-       btnArea.appendChild(button);
-      }
-      
-      
-      
-      // btnArea.appendChild(buttonTrue);
-      // btnArea.appendChild(buttonFalse);
+      });
+      btnArea.appendChild(button);
+    }
+
+    // btnArea.appendChild(buttonTrue);
+    // btnArea.appendChild(buttonFalse);
   }
-  
-  function answerVerify(user, correct) {  //controlla se le risposta è corretta
+
+  function answerVerify(user, correct) {
+    //controlla se le risposta è corretta
     if (user === correct) {
       correctAnswers++;
-   
     }
   }
   function theEnd() {
     window.location.href = "./resultspages.html";
   }
-  function shuffleArray(array) {   //  Fisher-Yates Shuffle: mescola gli array
+  function shuffleArray(array) {
+    //  Fisher-Yates Shuffle: mescola gli array
     for (let i = array.length - 1; i > 0; i--) {
-      
       const j = Math.floor(Math.random() * (i + 1)); //serve a generare un valore casuale.
       [array[i], array[j]] = [array[j], array[i]]; // destructuring assignnment: scambia i due elementi dell'array, ogni volta di un valore casuale (j)
-     
     }
     return array;
   }
-  
+
   //TO DO: collezionare il numero di risposte giuste in un array.
 };
 //that's all folks
+
+function stampaRisposte() {
+  console.log(correctAnswers);
+}
+stampaRisposte();
--- scripts/donutChart.js
+++ scripts/donutChart.js
@@ -1,26 +1,49 @@
-// Donut chart results page
-
-const rightAnswers = 9; // risposte giuste
-const worngAnswers = 1; // risposte sbagliate
+// SCRIPT DEL DONUT CHART
+const rightAnswers = 4;
+const wrongAnswers = 6; // risposte sbagliate
 const numberOfQuestions = 10; // numero domande
 
 const rightAnswersSlice = Math.floor((rightAnswers / numberOfQuestions) * 100); // percentuale delle risposte giuste senza simbolo %
-const wrongAnswersSlice = Math.floor((worngAnswers / numberOfQuestions) * 100); // percentuale delle risposte sbagliate senza simbolo %
-
-console.log(wrongAnswersSlice);
-console.log(rightAnswersSlice);
+const wrongAnswersSlice = Math.floor((wrongAnswers / numberOfQuestions) * 100); // percentuale delle risposte sbagliate senza simbolo %
 
 const percentageRightAnswers = (rightAnswers / numberOfQuestions).toLocaleString("en", {
   // percentuale delle risposte giuste con simbolo %
   style: "percent",
 });
-const percentageWrongAnswers = (worngAnswers / numberOfQuestions).toLocaleString("en", {
+const percentageWrongAnswers = (wrongAnswers / numberOfQuestions).toLocaleString("en", {
   // percentuale delle risposte sbagliate con simbolo %
   style: "percent",
 });
 
-console.log(percentageWrongAnswers);
-console.log(percentageRightAnswers);
-
 const circle = document.getElementById("donut-segment");
 circle.style.strokeDasharray = `${wrongAnswersSlice} ${rightAnswersSlice}`; // cambio delle porzioni del donut chart in base alla percentuale
+
+// CAMBIO PERCENTUALI AI LATI DEL DONUT CHART
+
+const percentualeCorrette = document.getElementById("percentageCorrect");
+const percentualeSbagliate = document.getElementById("percentageWrong");
+
+percentualeCorrette.innerText = percentageRightAnswers;
+percentualeSbagliate.innerText = percentageWrongAnswers;
+
+const giusteSuTutte = document.getElementById("giusteSuTot");
+const sbagliateSuTutte = document.getElementById("sbagliateSuTot");
+
+giusteSuTutte.innerText = `${rightAnswers}/${numberOfQuestions} questions`;
+sbagliateSuTutte.innerText = `${wrongAnswers}/${numberOfQuestions} questions`;
+
+const primaP = document.getElementById("firstCenterText");
+const secondaP = document.getElementById("secondCenterText");
+const terzaP = document.getElementById("thirdCenterText");
+
+if (rightAnswers >= 6) {
+  primaP.innerText = "Congratulations!";
+  secondaP.innerText = "You passed the exam.";
+} else {
+  primaP.innerText = "It wasn't enough!";
+  primaP.style.paddingTop = "3rem";
+  primaP.style.marginRight = "-1rem";
+  secondaP.innerText = "Maybe next time!";
+  secondaP.style.marginLeft = "1rem";
+  terzaP.innerText = "";
+}
