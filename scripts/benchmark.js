6867a50b6db16d33887e808536a59fb5bc6d4c35
Dan Russo <rdaniele669@gmail.com>
2025-04-04 14:48:04 +0200

    adds fixes

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -131,7 +131,7 @@ window.onload = function () {
         }
         loadQuestion();
 
-        // Forza il reflow (rende l'elemento visibile di nuovo)
+        // forza il reflow (rende l'elemento visibile di nuovo)
         void progressBar.offsetHeight;
 
         setTimeout(() => {
@@ -144,7 +144,7 @@ window.onload = function () {
       }
     }
 
-    updateTimer(); // Avvia il timer
+    updateTimer(); // avvia il timer
   }
 
   function answerVerify(user, correct) {
@@ -164,30 +164,4 @@ window.onload = function () {
     }
     return array;
   }
-
-  //TO DO: collezionare il numero di risposte giuste in un array.
 };
-//that's all folks
-
-// const sec = 30;
-
-// const setTime = sec * 1000; // trasforma i secondi in millisec
-// const startTime = Date.now(); // prende i millesec dal 01/01/70
-// const futureTime = startTime + setTime; //somma i due valori sopra
-
-// const countDownTimer = function () {
-//   const currentTime = Date.now(); // riprende i millisec dal 01/01/70, ma essendo dentro la funzione che si ripete a loop, cambiano col passare dei secondi reali
-//   const remainingTime = futureTime - currentTime; // calcola la differenza col tempo che passa
-//   const timerText = document.getElementById("middleText"); // selezioniamo la variabile del testo
-
-//   if (remainingTime <= 0) {
-//     // se i tempo rimanente è uguale a 0
-//     clearInterval(timerLoop); // Ferma il timer
-//     timerText.innerText = "00"; // Timer scaduto quindi rimettiamo 00 sul testo
-//   } else {
-//     const secs = Math.floor((remainingTime / 1000) % 60).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
-//     timerText.innerText = secs; // Mostra il tempo rimanente
-//   }
-// };
-
-// const timerLoop = setInterval(countDownTimer);
